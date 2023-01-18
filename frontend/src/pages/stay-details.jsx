609aeb5f94a313ee12a466d33a99c80a08613a59
Elichay Kaplan <Elichay_Kaplan@Yahoo.com>
2023-01-18 18:12:06 +0200

    util-cmps

--- frontend/src/pages/stay-details.jsx
+++ frontend/src/pages/stay-details.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 
 import { stayService } from '../services/stay.service.local.js'
-import { RatingReview } from '../cmps/stay-details/rating-review.jsx'
+import { RatingReview } from '../cmps/util-cmps/rating-review.jsx'
 
 
 
