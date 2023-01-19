5ef3d39240d1718923d9d4485e9cb76e500230c5
Andrey Solonikov <andreysol@yahoo.com>
2023-01-19 08:38:39 +0200

    routing

--- frontend/src/cmps/stay-list/stay-list.jsx
+++ frontend/src/cmps/stay-list/stay-list.jsx
@@ -1,13 +1,24 @@
 import { StayPreview } from './stay-preview';
+import { useNavigate } from 'react-router-dom';
 
-export function StayList({stays}) {
+export function StayList({ stays }) {
+  const navigate = useNavigate();
+
+  if (!stays)
+    <ul className='card-grid stay-list clean-list main-layout'>Loading..</ul>;
   return (
     <ul className='card-grid stay-list clean-list main-layout'>
-      {stays.map((stay) => (
-        <li key={stay._id} className='stay-list-item'>
-          <StayPreview stay={stay} />
-        </li>
-      ))}
+      {stays.map((stay) => {
+        console.log('stay:', stay);
+        return (
+          <li
+            key={stay._id}
+            onClick={() => navigate(`/stay/${stay._id}`)}
+            className='stay-list-item'>
+            <StayPreview stay={stay} />
+          </li>
+        );
+      })}
     </ul>
   );
 }
