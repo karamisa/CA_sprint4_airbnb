58f09c96fd5041c7f792a129f3664f40c2319561
Elichay Kaplan <Elichay_Kaplan@Yahoo.com>
2023-01-19 08:31:05 +0200

    className

--- frontend/src/cmps/guest-select.jsx
+++ frontend/src/cmps/guest-select.jsx
@@ -1,89 +1,89 @@
 export function GuestSelect() {
     return (
         <div className="guest-select">
-            <div class="guest-select-adult flex justify-between">
-                <div class="guest-select-label">
-                    <div class="guest-select-adults">
+            <div className="guest-select-adult flex justify-between">
+                <div className="guest-select-label">
+                    <div className="guest-select-adults">
                         Adults
                     </div>
-                    <div class="adults-subheader">
+                    <div className="adults-subheader">
                         Ages 13 or above
                     </div>
                 </div>
-                    <div class="guest-select-stepper-btn flex">
-                        <button class="stepper-btn">
+                    <div className="guest-select-stepper-btn flex">
+                        <button className="stepper-btn">
                             +
                         </button>
-                        <div class="stepper-value">
+                        <div className="stepper-value">
                             1
                         </div>
-                        <button class="stepper-btn">
+                        <button className="stepper-btn">
                           - 
                         </button>
                     </div>
             </div>
-            <div class="guest-select-children flex justify-between">
-                <div class="guest-select-label">
-                    <div class="guest-select-adults">
+            <div className="guest-select-children flex justify-between">
+                <div className="guest-select-label">
+                    <div className="guest-select-adults">
                         Children
                     </div>
-                    <div class="adults-subheader">
+                    <div className="adults-subheader">
                         Ages 2-12
                     </div>
                 </div>
 
-                    <div class="guest-select-stepper-btn flex">
-                        <button class="stepper-btn">
+                    <div className="guest-select-stepper-btn flex">
+                        <button className="stepper-btn">
                             +
                         </button>
-                        <div class="stepper-value">
+                        <div className="stepper-value">
                             1
                         </div>
-                        <button class="stepper-btn">
+                        <button className="stepper-btn">
                           -
                         </button>
                     </div>
             </div>
-            <div class="guest-select-infants flex justify-between">
-                <div class="guest-select-label">
-                    <div class="guest-select-adults">
+            <div className="guest-select-infants flex justify-between">
+                <div className="guest-select-label">
+                    <div className="guest-select-adults">
                         Infants
                     </div>
-                    <div class="adults-subheader">
+                    <div className="adults-subheader">
                         Under 2
                     </div>
                 </div>
 
-                    <div class="guest-select-stepper-btn flex">
-                        <button class="stepper-btn" type="button" aria-label="Decrease number of adults">
+                    <div className="guest-select-stepper-btn flex">
+                        <button className="stepper-btn" type="button" aria-label="Decrease number of adults">
                             +
                         </button>
-                        <div class="stepper-value">
+                        <div className="stepper-value">
                             1
                         </div>
-                        <button class="stepper-btn" type="button" aria-label="Increase number of adults">
+                        <button className="stepper-btn" type="button" aria-label="Increase number of adults">
                           -
                         </button>
                     </div>
             </div>
-            <div class="guest-select-pets flex justify-between" data-testid="search-block-filter-stepper-row-stepper-adults">
-                <div class="guest-select-label">
-                    <div class="guest-select-adults">
+            <div className="guest-select-pets flex justify-between" data-testid="search-block-filter-stepper-row-stepper-adults">
+                <div className="guest-select-label">
+                    <div className="guest-select-adults">
                         Pets
                     </div>
-                    <div class="adults-subheader">
+                    <div className="adults-subheader">
                         <a href="">Bringing a Service Animal?</a>
                     </div>
                 </div>
 
-                    <div class="guest-select-stepper-btn flex">
-                        <button class="stepper-btn" type="button" aria-label="Decrease number of adults">
+                    <div className="guest-select-stepper-btn flex">
+                        <button className="stepper-btn" type="button" aria-label="Decrease number of adults">
                             +
                         </button>
-                        <div class="stepper-value">
+                        <div className="stepper-value">
                             1
                         </div>
-                        <button class="stepper-btn" type="button" aria-label="Increase number of adults">
+                        <button className="stepper-btn" type="button" aria-label="Increase number of adults">
                           -
                         </button>
                     </div>
