5d3b7b6b48cc29f75daedf24039bda550a851365
Andrey Solonikov <andreysol@yahoo.com>
2023-01-26 16:45:27 +0200

    dashboard

--- frontend/src/cmps/hosting/dashboard-page.jsx
+++ frontend/src/cmps/hosting/dashboard-page.jsx
@@ -1,9 +1,12 @@
+import { ChartInfo } from '../ui/chart-info'
 import { ChartGuest } from './charts/chart-guest'
 import { ChartIncome } from './charts/chart-income'
 import { ChartOccupancy } from './charts/chart-occupancy'
 import { ChartRating } from './charts/chart-rating'
 
 export function DashboardPage() {
+  const infoIncome = { title: 'Income', amount: 3643, lastMonthAmount: 15 }
+  const infoLike = { title: 'Likes', amount: 4, lastMonthAmount: -3 }
   return (
     <div className='dashboard-page secondary-layout'>
       <div className='hero'>
@@ -11,11 +14,13 @@ export function DashboardPage() {
       </div>
       <h3>Dashboard</h3>
       <div className='dashboard'>
+        <ChartInfo info={infoIncome} />
+        <ChartInfo info={infoLike} />
         <ChartOccupancy />
         <ChartRating />
         <ChartIncome />
         <ChartGuest />
       </div>
     </div>
-  );
+  )
 }
---
+++ frontend/src/cmps/ui/chart-info.jsx
@@ -0,0 +1,14 @@
+export function ChartInfo({ info }) {
+  const { amount, lastMonthAmount, title, icon } = info
+  return (
+    <article className='chart-info chart-rectangle'>
+      <div className='image-wrapper'>{icon}</div>
+      <h4 className='chart-info-title'>{title}</h4>
+      <span className='chart-info-amount'>{amount}</span>
+      <p>
+        <span className='chart-info-last-amount'>{lastMonthAmount}</span> %
+        since last month
+      </p>
+    </article>
+  )
+}
