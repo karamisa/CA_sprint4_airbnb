2d2eb8395da4a49c85edf1488825d4cc8a589dfb
Karam Isa <karam_isa@icloud.com>
2023-01-30 12:54:03 +0200

    backend fix: add missing dependency

--- backend/config/dev.js
+++ backend/config/dev.js
@@ -1,4 +1,4 @@
 module.exports = {
-  dbURL:'mongodb+srv://karamisa:44qCoS0XiDKgGPoO@akebnb.z1d615a.mongodb.net/?retryWrites=true&w=majority',
-  dbName:'akebnb',
+  dbURL:'mongodb+srv://karamisa:44qCoS0XiDKgGPoO@akebnb.z1d615a.mongodb.net/?retryWrites=true&w=majority&authSource=admin',
+  dbName:'akebnb'
 }
