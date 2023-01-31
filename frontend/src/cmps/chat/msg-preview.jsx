229eb8ec008abfe3bb9ee9700544c80fb13a206c
Elichay Kaplan <Elichay_Kaplan@Yahoo.com>
2023-01-31 11:13:34 +0200

    msgs

--- frontend/src/cmps/chat/msg-preview.jsx
+++ frontend/src/cmps/chat/msg-preview.jsx
@@ -1,13 +1,14 @@
-export function MsgPreview({order}){
-    
-    return  (
+export function MsgPreview({ order }) {
+
+  return (
 
     <div className="msg-preview">
-    <div className="msg-avatar">
-      <img src={order.imgUrl} alt={'avatar'} className="mini-user-img" />
+      <div className="msg-avatar">
+        <img src={order.imgUrl} alt={'avatar'} className="mini-user-img" />
+      </div>
+      <div className="msg-name">{order.fullname}</div>
+      <div className="msg-txt">{order.msgs[order.msgs.length - 1]?.txt}</div>
+      {/* <div className="msg-txt">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorem fugiat inventore nam, necessitatibus iure fugit unde eos esse quam repudiandae amet ipsam expedita reprehenderit ab quasi tempore id nesciunt magnam?</div> */}
     </div>
-    <div className="msg-name">{order.fullname}</div>
-    <div className="msg-txt">{order.msgs[order.msgs.length - 1]?.txt}</div>
-    {/* <div className="msg-txt">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorem fugiat inventore nam, necessitatibus iure fugit unde eos esse quam repudiandae amet ipsam expedita reprehenderit ab quasi tempore id nesciunt magnam?</div> */}
-  </div>
-    )}
+  )
+}
