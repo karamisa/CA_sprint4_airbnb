0e9c7c82a98eed79a3877189a0563358cf9edeae
Andrey Solonikov <andreysol@yahoo.com>
2023-01-31 13:04:05 +0200

    messages

--- frontend/src/pages/messages-page.jsx
+++ frontend/src/pages/messages-page.jsx
@@ -34,74 +34,78 @@ export function MessagesPage() {
   return (
     <section className='messages-page'>
       {/* HEADER */}
-      <div className='header-container'>
-        <header className='app-header main-layout flex'>
-          <div className='header-logo-container'>
-            <Logo />
-          </div>
-          <div className='spacer'></div>
-          <div className='header-menu-container'>
-            <NavMenu />
-          </div>
-        </header>
-      </div>
+      {/* <div className='header-container'> */}
+      <header className='app-header header-container flex'>
+        <div className='header-logo-container'>
+          <Logo />
+        </div>
+        <div className='spacer'></div>
+        <div className='header-menu-container'>
+          <NavMenu />
+        </div>
+      </header>
+      {/* </div> */}
 
       {/* MAIN 3 Columns {orderList - messages - orderDetails} */}
-      <div className='msg-main-container main-layout'>
-      <div className='inbox-container'>
-        {/* Column 1 - Orders List/Select */}
-        <div className='inbox-column orders-list'>
-          <div className='inbox-header all-orders'>
-            <h2>Orders messages</h2>
-          </div>
-          {isLoading && (
-            <div className='inbox-list'>
-              <h1 style={{ textAlign: 'center', marginTop: '20px' }}>
-                Loading...
-              </h1>
+      <div className='msg-main-container'>
+        <div className='inbox-container'>
+          {/* Column 1 - Orders List/Select */}
+          <div className='inbox-column orders-list'>
+            <div className='inbox-header all-orders'>
+              <h2>Orders messages</h2>
             </div>
-          )}
-          {!isLoading && (
-            <div className='inbox-list-container'>
-              <ChatOrderList
-                orders={orders}
-                loggedInUser={loggedInUser}
-                onSetCurrOrder={onSetCurrOrder}
-                currOrder={currOrder}
-              />
-            </div>
-          )}
-        </div>
+            {isLoading && (
+              <div className='inbox-list'>
+                <h1 style={{ textAlign: 'center', marginTop: '20px' }}>
+                  Loading...
+                </h1>
+              </div>
+            )}
+            {!isLoading && (
+              <div className='inbox-list-container'>
+                <ChatOrderList
+                  orders={orders}
+                  loggedInUser={loggedInUser}
+                  onSetCurrOrder={onSetCurrOrder}
+                  currOrder={currOrder}
+                />
+              </div>
+            )}
+          </div>
 
-        {/* Column 2 - Messages */}
-        <div className='inbox-column messages'>
-          <div className='chat-backdrop'>
-          {isLoading &&  <h1 style={{ textAlign: 'center', marginTop: '20px' }}>Loading...</h1>}
-          {!isLoading && currOrder && (
-            <div className={'chat-container'}>
-            <ChatRoom order={currOrder} loggedInUser={loggedInUser} />
-          </div>
-          )}
-          </div>
-        </div>
-
-        {/* Column 3 - Order Details */}
-        <div className='inbox-column order-details'>
-          <div className='inbox-header'>
-            <h2>Order details</h2>
-          </div>
-          {currOrder && (
-            <div className='currorder-details'>
-              {isLoading && <div>Loading...</div>}
-              {!isLoading && (
-                <>
-                  <ChatOrderDetails currOrder={currOrder} />
-                </>
+          {/* Column 2 - Messages */}
+          <div className='inbox-column messages'>
+            <div className='chat-backdrop'>
+              {isLoading && (
+                <h1 style={{ textAlign: 'center', marginTop: '20px' }}>
+                  Loading...
+                </h1>
+              )}
+              {!isLoading && currOrder && (
+                <div className={'chat-container'}>
+                  <ChatRoom order={currOrder} loggedInUser={loggedInUser} />
+                </div>
               )}
             </div>
-          )}
+          </div>
+
+          {/* Column 3 - Order Details */}
+          <div className='inbox-column order-details'>
+            <div className='inbox-header'>
+              <h2>Order details</h2>
+            </div>
+            {currOrder && (
+              <div className='currorder-details'>
+                {isLoading && <div>Loading...</div>}
+                {!isLoading && (
+                  <>
+                    <ChatOrderDetails currOrder={currOrder} />
+                  </>
+                )}
+              </div>
+            )}
+          </div>
         </div>
-      </div>
       </div>
     </section>
   )
